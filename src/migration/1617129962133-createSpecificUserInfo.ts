3431d84135b383c73f0683b153132b29c11660f1
PSouza-10 <paulosouza300272@gmail.com>
2021-03-31 13:48:16 -0300

    removed emailVerified from migration

--- src/migration/1617129962133-createSpecificUserInfo.ts
+++ src/migration/1617129962133-createSpecificUserInfo.ts
@@ -4,14 +4,14 @@ export class createSpecificUserInfo1617129962133 implements MigrationInterface {
 	name = 'createSpecificUserInfo1617129962133'
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(
-			`ALTER TABLE "user" ADD "emailVerified" boolean NOT NULL DEFAULT false`
-		)
+		// await queryRunner.query(
+		// 	`ALTER TABLE "user" ADD "emailVerified" boolean NOT NULL DEFAULT false`
+		// )
 		await queryRunner.query(`ALTER TABLE "user" ADD "anamnesis" jsonb`)
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
 		await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "anamnesis"`)
-		await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "emailVerified"`)
+		// await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "emailVerified"`)
 	}
 }
